1a1b6cf073108bfec09a20131fa6491e34a734fc
Alexstericris <42732998+Alexstericris@users.noreply.github.com>
2018-11-08 13:15:35 +0100

    Update Kunde.java

--- Kunde.java
+++ Kunde.java
@@ -2,7 +2,9 @@ public class Kunde{
   private Adresse anschrift;
   private String firma;
   private Auftrag[] aufträge=new Auftrag[30];
+  private ArrayList<Auftrag> aufträge=new ArrayList<Auftrag>();
   
-  public void addAuftrag(
-
+  public Kunde(Adresse anschrift1, String firma,){
+  public void addAuftrag();
+  }
 }
